538c81dbbddf221d32ea18330b5e56d5afaf6d2d
Oleg Matskiv <omatskiv@redhat.com>
2017-06-05 14:12:27 +0200

    Initial structure

---
+++ src/main/java/org/aerogear/digger/test/App.java
@@ -0,0 +1,13 @@
+package org.aerogear.digger.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
